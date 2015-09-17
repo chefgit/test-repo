1532f1e4a8a22e4fb6e08b2c2f4035344c974ff0
fredtaku <fredtaku@gmail.com>
2015-09-16 19:31:16 -0500

    Added a line to print out

--- HelloWorld/Hello.java
+++ HelloWorld/Hello.java
@@ -4,6 +4,9 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 
+		
+		
+		System.out.println("this is just a test of git from eclipse");
 	}
 
 }
